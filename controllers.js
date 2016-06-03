7331387fc59611110a5e4afba270736856e487c7
Pritam De <pritam.de@teamaol.com>
2016-06-03 09:27:58 -0700

    With a Minor change

--- controllers.js
+++ controllers.js
@@ -19,7 +19,7 @@ weatherApp.controller('forecastController', ['$scope', '$location', '$log', '$re
     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
     $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city,
                                                   cnt: $scope.days,
-                                                  appid: "027b42cdea4dedcac11ba374c62b7996"});
+                                                  appid: "<API ID>"});
     $scope.convertToFahrenheit = function(degK) {
         return (Math.round((1.8 * (degK - 273)) + 32));
     };
