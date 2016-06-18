dff8f943d5d4851150e2e28d0ab92a4cc0b48e6a
Pritam De <pritam.de@teamaol.com>
2016-06-18 13:28:40 -0700

    Added unit testings

--- test/unittest/unittest.js
+++ test/unittest/unittest.js
@@ -1,58 +1,67 @@
+'use strict';
+
 // Jasmine unit tests
-
-describe('Hello message:', function(){
+describe('Weather Forecast App:', function(){
     
     beforeEach(module('weatherApp'));
-    
-  var $controller;
 
-  beforeEach(inject(function(_$controller_){
-    // The injector unwraps the underscores (_) from around the parameter names when matching
-    $controller = _$controller_;
-  }));   
-    
-    
-      
-    
-    beforeEach(inject(function(){
-        var homeCtrl; 
-        homeCtrl = $controller('homeController');
-    }));    
-    
+    var $controller;
+    var $rootScope;
+    var cityService;
+    var weatherAppService;
 
-    it('should be green', function(){
-        var green = 'green';
-        console.log(green);
-        alert(green);
-        expect(green).toBe('green');
+    beforeEach(inject(function(_$controller_, _$rootScope_, _cityService_, _weatherAppService_){
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $controller = _$controller_;
+        $rootScope = _$rootScope_;
+        cityService = _cityService_;
+        weatherAppService = _weatherAppService_;
+    }));   
+    
+    var homeCtrl; 
+    var $scope = {};
+    var forecastCtrl;
+    
+    // instantiate controllers
+    beforeEach(function(){
+            $scope = $rootScope.$new();
+            homeCtrl = $controller('homeController', { $scope: $scope });   
+            forecastCtrl = $controller('forecastController', { $scope: $scope });  
+    });
+    
+    // unit tests for Home controller
+    describe(' Home Controller:', function(){
+        it('should validate options', function(){
+            expect($scope.options).toEqual({
+                                        country: 'us',
+                                        types: '(cities)'
+                                        });
+        });
         
-    });
+        it('should validate the default city setting', function(){
+            expect(cityService.city).toEqual("Redmond, WA, United States");
+        });
+    });  
     
+    // unit tests for Weather App Service controller
+    describe(' Weather App Service Controller:', function(){
+       it('should validate default days value', function(){
+            expect($scope.days).toBe('2');
+       });
+        
+        it('should validate convertToFahrenheit():', function(){
+            expect($scope.convertToFahrenheit(290)).toBe(63); 
+            expect($scope.convertToFahrenheit(300)).toBe(81); 
+            expect($scope.convertToFahrenheit(273)).toBe(32);   
+            expect($scope.convertToFahrenheit(241)).toBe(-26); 
+            expect($scope.convertToFahrenheit(250)).toBe(-9); 
+        });
+        
+        it('should validate convertToDate():', function(){
+            var expectedDate = $scope.convertToDate(1000000000);
+            expect(expectedDate.getDate()).toEqual(8);
+            expect(expectedDate.getDay()).toEqual(6);
+            expect(expectedDate.getFullYear()).toEqual(2001);
+        });
+    });     
 });
- 
-
-/*
-describe('Controller: homeController', function() {
-   
-    beforeEach(module('weatherApp'));
-    
-    var homeCtrl;
-    
-    // Instantiate a new instance of home controller 
-    beforeEach(inject(function($controller){
-        homeCtrl = $controller('homeController');
-    }));
-    
-    it('it should be green', function(){
-       
-        var message = 'green';
-        expect(message).toBe('green');
-    });
-
-    it('should have the city set correctly', function(){
-       expect(homeCtrl.testdata).toEqual([
-        {id:1, label: 'First'},
-        {id:2, label: 'Second'}           
-       ]); 
-    });    
-});  */
